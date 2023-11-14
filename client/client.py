9c1f2ed11a9ae946caf64858301c793844654804
Emma St. Laurent <83246818+stlaurentel@users.noreply.github.com>
2023-11-13 21:19:30 -0700

    Update client.py

--- client/client.py
+++ client/client.py
@@ -11,30 +11,31 @@ GPIO.setup(buttonPin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 
 # Create an ArgumentParser to parse the command-line arguments
-parser = argparse.ArgumentParser(description='Bluetooth client script')
-parser.add_argument('server_address', type=str, help='Server Bluetooth device address')
-parser.add_argument('port', type=int, help='Port number to connect to')
-args = parser.parse_args()
+#parser = argparse.ArgumentParser(description='Bluetooth client script')
+#parser.add_argument('server_address', type=str, help='Server Bluetooth device address')
+# parser.add_argument('port', type=int, help='Port number to connect to')
+# args = parser.parse_args()
 
 # Retrieve the server address and port from the command-line arguments
-server_address = args.server_address        # format: "XX:XX:XX:XX:XX:XX"
-port = args.port
+server_address = "XX:XX:XX:XX:XX:XX"        # format: "XX:XX:XX:XX:XX:XX"
+port = 1
 
 client_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
 try:
-    buttonState = GPIO.input(buttonPin))
-    while buttonState != 1:
-        buttonState = GPIO.input(buttonPin))
+    buttonState = GPIO.input(buttonPin)
+    while buttonState != 0:
+        buttonState = GPIO.input(buttonPin)
+        print(buttonState)
     
     client_socket.connect((server_address, port))
 except bluetooth.btcommon.BluetoothError as e:
     print(f"Bluetooth error: {e}")
 
 # For each piece of data read from sensor, send the data
-dataLists = sensorReader.readHeartData(15)
+dataLists = sensorReader.readHeartData(1)
 for data in dataLists:
-    client_socket.send(np.array2string(data).encode())
+    client_socket.send(np.array2string(data,separator=",").encode())
 
 data = client_socket.recv(1024)
 print(f"Received: {data}")
