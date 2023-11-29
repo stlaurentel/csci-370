c599b3b2f2a6ca97e16c7560c7df88237ef5f363
Emma St. Laurent <83246818+stlaurentel@users.noreply.github.com>
2023-11-28 19:50:48 -0700

    Update client.py

--- client/client.py
+++ client/client.py
@@ -6,9 +6,10 @@ import RPi.GPIO as GPIO
 
 # use GPIO numbering
 GPIO.setmode(GPIO.BCM)
-buttonPin = 12
+button_pin = 12
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
+CHUNK_SIZE = 1024
 
 # Create an ArgumentParser to parse the command-line arguments
 #parser = argparse.ArgumentParser(description='Bluetooth client script')
@@ -23,19 +24,25 @@ port = 1
 client_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
 try:
-    buttonState = GPIO.input(buttonPin)
-    while buttonState != 0:
-        buttonState = GPIO.input(buttonPin)
-        print(buttonState)
-    
+    button_state = GPIO.input(button_pin)
+    print("Awaiting input...")
+    while button_state != 0:
+        button_state = GPIO.input(button_pin)
+    print("Button pressed! Collecting data now...")
     client_socket.connect((server_address, port))
 except bluetooth.btcommon.BluetoothError as e:
     print(f"Bluetooth error: {e}")
 
 # For each piece of data read from sensor, send the data
-dataLists = sensorReader.readHeartData(1)
-for data in dataLists:
-    client_socket.send(np.array2string(data,separator=",").encode())
+data_lists = sensorReader.readHeartData(30)
+for data in data_lists:
+    data_str = np.array2string(data,separator=",")
+    bytes_to_send = data_str.encode()
+
+    for i in range(0, len(bytes_to_send), CHUNK_SIZE):
+        client_socket.send(bytes_to_send[i:i + CHUNK_SIZE])
+
+client_socket.send("$$$")
 
 data = client_socket.recv(1024)
 print(f"Received: {data}")
